c05f400ae4932e38db658fca693920994913c833
VladimirDmitrienko <dmitrienkovladimir97@gmail.com>
2023-08-27 00:27:45 +0300

    build: add Gradle build files
    Refs: #1.

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+}
+
+group = "org.koto-project"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
